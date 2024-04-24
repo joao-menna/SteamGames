cb3939afd20f2bec3069e7edc48a4e366ad3bbff
joao-menna <joao.menna1907@gmail.com>
2024-04-24 14:31:26 -0300

    adicionado variancia aos calculos

--- calculate.py
+++ calculate.py
@@ -9,6 +9,8 @@ with open("./games_cleared.json", encoding="utf-8") as file:
 moda = lambda x: max(set(x), key=x.count)
 media = lambda x: np.mean(x)
 mediana = lambda x: np.median(x)
+variancia_amostral = lambda x: np.var(x, ddof=1)
+variancia_populacional = lambda x: np.var(x)
 dp_amostral = lambda x: np.std(x, ddof=1)
 dp_populacional = lambda x: np.std(x)
 cv_amostral = lambda x: np.std(x, ddof=1) / np.mean(x) * 100
@@ -18,11 +20,13 @@ all_net_revenue = []
 for game in games:
     all_net_revenue.append(game["netRevenue"])
 
-games_moda = moda(all_net_revenue)
 games_minimo = min(all_net_revenue)
 games_maximo = max(all_net_revenue)
+games_moda = moda(all_net_revenue)
 games_media = media(all_net_revenue)
 games_mediana = mediana(all_net_revenue)
+games_var_amostral = variancia_amostral(all_net_revenue)
+games_var_populacional = variancia_populacional(all_net_revenue)
 games_dp_amostral = dp_amostral(all_net_revenue)
 games_dp_populacional = dp_populacional(all_net_revenue)
 games_cv_amostral = cv_amostral(all_net_revenue)
@@ -33,14 +37,16 @@ jogo_maximo = games[all_net_revenue.index(games_maximo)]
 
 with open("./dados.json", "w+") as file:
     file_contents = json.dumps({
-        "moda": games_moda,
         "minimo": games_minimo,
         "maximo": games_maximo,
         "amplitude": games_maximo - games_minimo,
         "jogoDoMinimo": jogo_minimo,
         "jogoDoMaximo": jogo_maximo,
+        "moda": games_moda,
         "media": games_media,
         "mediana": games_mediana,
+        "varianciaAmostral": games_var_amostral,
+        "varianciaPopulacional": games_var_populacional,
         "desvioPadraoAmostral": games_dp_amostral,
         "desvioPadraoPopulacional": games_dp_populacional,
         "coeficienteVariacaoAmostral": games_cv_amostral,
