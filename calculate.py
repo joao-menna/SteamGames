ca2f84588e6f1058d588446e38f3a63c4755b73e
joao-menna <joao.menna1907@gmail.com>
2024-04-24 14:08:07 -0300

    corrigido clear_free_games e adicionado moda ao calculate

--- calculate.py
+++ calculate.py
@@ -6,10 +6,11 @@ with open("./games_cleared.json", encoding="utf-8") as file:
     file_contents = file.read()
     games = json.loads(file_contents)
 
+moda = lambda x: max(set(x), key=x.count)
 media = lambda x: np.mean(x)
 mediana = lambda x: np.median(x)
 dp_amostral = lambda x: np.std(x, ddof=1)
-dp_populacional = lambda x: np.std(x, ddof=1)
+dp_populacional = lambda x: np.std(x)
 cv_amostral = lambda x: np.std(x, ddof=1) / np.mean(x) * 100
 cv_populacional = lambda x: np.std(x) / np.mean(x) * 100
 
@@ -17,6 +18,7 @@ all_net_revenue = []
 for game in games:
     all_net_revenue.append(game["netRevenue"])
 
+games_moda = moda(all_net_revenue)
 games_minimo = min(all_net_revenue)
 games_maximo = max(all_net_revenue)
 games_media = media(all_net_revenue)
@@ -28,6 +30,7 @@ games_cv_populacional = cv_populacional(all_net_revenue)
 
 with open("./dados.json", "w+") as file:
     file_contents = json.dumps({
+        "moda": games_moda,
         "minimo": games_minimo,
         "maximo": games_maximo,
         "amplitude": games_maximo - games_minimo,
--- clear_free_games.py
+++ clear_free_games.py
@@ -5,11 +5,12 @@ with open("./games_with_revenue.json", encoding="utf-8", errors="ignore") as fil
     file_contents = file.read()
     games = json.loads(file_contents)
 
+games_cleared = []
 for game in games:
-    if game["price"] != 0:
+    if game["price"] == 0:
         continue
 
-    games.remove(game)
+    games_cleared.append(game)
 
 with open("./games_cleared.json", "w+") as file:
-    file.write(json.dumps(games, indent=2))
+    file.write(json.dumps(games_cleared, indent=2))
